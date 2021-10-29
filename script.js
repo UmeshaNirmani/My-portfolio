df3942d96af8dd90cf82520e2a51cc1bf7efc89e
Umesha-Nirmani <58287574+Umesha-Nirmani@users.noreply.github.com>
2021-10-30 03:34:41 +0530

    Completed home page

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-window.addEventListener("scroll", () => {
-  const counters = document.querySelectorAll(".counter");
-  const speed = 10000000000;
+// window.addEventListener("scroll", () => {
+//   const counters = document.querySelectorAll(".counter");
+//   const speed = 10000000000;
 
-  counters.forEach((counter) => {
-    const updateCount = () => {
-      const target = +counter.getAttribute("data-target");
-      const cnt = +counter.innerText;
+//   counters.forEach((counter) => {
+//     const updateCount = () => {
+//       const target = +counter.getAttribute("data-target");
+//       const cnt = +counter.innerText;
 
-      const inc = target / speed;
+//       const inc = target / speed;
 
-      console.log(cnt);
+//       console.log(cnt);
 
-      if (cnt < target) {
-        counter.innerText = Math.ceil(cnt + inc);
-        setTimeout(updateCount, 1);
-      } else {
-        cnt.innerText = target;
-      }
-    };
+//       if (cnt < target) {
+//         counter.innerText = Math.ceil(cnt + inc);
+//         setTimeout(updateCount, 1);
+//       } else {
+//         cnt.innerText = target;
+//       }
+//     };
 
-    updateCount();
+//     updateCount();
+//   });
+// });
+
+document
+  .getElementById("btn1")
+  .addEventListener("click", function () {
+    document.getElementsByClassName("slider")[0].classList.add("active");
   });
+
+document.getElementById("close").addEventListener("click", function () {
+  document.getElementsByClassName("slider")[0].classList.remove("active");
 });
