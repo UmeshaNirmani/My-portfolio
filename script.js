6e6aadde0e66c505e2813e105d824533755f7f68
Umesha-Nirmani <58287574+Umesha-Nirmani@users.noreply.github.com>
2021-10-29 11:19:16 +0530

    created the counter

---
+++ script.js
@@ -0,0 +1,22 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 1000000000;
+
+counters.forEach((counter) => {
+  const updateCount = () => {
+    const target = +counter.getAttribute("data-target");
+    const cnt = +counter.innerText;
+
+    const inc = target / speed;
+
+    console.log(cnt);
+
+    if (cnt < target) {
+      counter.innerText = Math.ceil(cnt + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      cnt.innerText = target;
+    }
+  };
+
+  updateCount();
+});
